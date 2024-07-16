21b982030168fd3387e09fee87af05e061c57bbc
Bisrat Kebere <kiyyakebere@gmail.com>
2024-07-17 00:40:42 +0300

    curried function example

---
+++ curried.js
@@ -0,0 +1,1 @@
+// 1.Implement a curried function that calculates the volume of a rectangular prism. Then use it to create a partially applied function that calculates the volume for a specific height.
