29990c0de1a3bf1f0ab030c7ac0fe1794e659c71
Bisrat Kebere <kiyyakebere@gmail.com>
2024-07-17 00:41:56 +0300

    implement pure function example

--- pure.js
+++ pure.js
@@ -1,1 +1,12 @@
 // 2.Write a pure function that takes a nested array and flattens it. Ensure the original array remains unchanged use recursion and functional programming principles.
+
+function flattenArray(arr) {
+    return arr.reduce((acc, item) => {
+        return acc.concat(Array.isArray(item) ? flattenArray(item) : item);
+    }, []);
+}
+
+const nestedArray = [1, [2, 4, 5]];
+console.log(nestedArray);
+console.log(flattenArray(nestedArray));
+console.log(nestedArray);
